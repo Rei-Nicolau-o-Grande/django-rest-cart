ec1b2249ba18cfa9cac2bd4670b02997ae8e62f4
Monstro <nicolasmeireles1004@gmail.com>
2023-02-12 17:30:47 -0300

    terminando o carrinho

--- product/admin.py
+++ product/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Cart, ItemCart
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'price', 'score', 'image'
     list_display_links = 'name',
     search_fields = 'name', 'price', 'score'
 
+class CartAdmin(admin.ModelAdmin):
+    list_display = 'id', 'user', 'shipping_cost', 'subtotal', 'total', 'created_at', 'updated_at',
+    list_display_links = 'id', 'user'
+
+class ItemCartAdmin(admin.ModelAdmin):
+    list_display = 'id', 'product', 'quantity', 'total_price', 'created_at', 'updated_at'
+    list_display_links = 'id', 'product'
+
 
 admin.site.register(Product, ProductAdmin)
+
+admin.site.register(Cart, CartAdmin)
+
+admin.site.register(ItemCart, ItemCartAdmin)
--- product/views.py
+++ product/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializers
+from .models import Product, Cart, ItemCart
+from .serializers import ProductSerializer, CartSerializer, ItemCartSerializer
 
 from .pagination import Pagination
 
@@ -14,9 +17,48 @@ from .filters import ProductFilter
 
 class ListProduct(generics.ListAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializers
+    serializer_class = ProductSerializer
     pagination_class = Pagination
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, ]
 
     filter_backends = (DjangoFilterBackend,)
     filterset_class = ProductFilter
+
+class CartApi(ModelViewSet):
+
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    lookup_field = 'user'
+
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Cart.objects.filter(user=user)
+        return queryset
+
+
+@api_view(http_method_names=['post'])
+@permission_classes(permission_classes=[IsAuthenticated])
+def item_cart_post(request):
+
+    if request.method == 'POST':
+        serializer = ItemCartSerializer(
+            data=request.data,
+            context={'request': request},
+        )
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(http_method_names=['delete'])
+@permission_classes(permission_classes=[IsAuthenticated])
+def item_cart_delete(request, pk):
+    qs = get_object_or_404(ItemCart, pk=pk)
+
+    if request.method == 'DELETE':
+        qs.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
